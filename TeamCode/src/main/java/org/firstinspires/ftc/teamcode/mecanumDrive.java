3ace6c2abcd93c03161c175dbbe0587ff8a021e4
OwenBachyrycz <60482509+OwenBachyrycz@users.noreply.github.com>
2021-11-17 16:33:08 -0500

    Added empty TeleOp for mecanum drivetrain

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -0,0 +1,20 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Create by Owen Bachyrycz on 11/17/2021.
+ */
+
+public class mecanumDrive extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
