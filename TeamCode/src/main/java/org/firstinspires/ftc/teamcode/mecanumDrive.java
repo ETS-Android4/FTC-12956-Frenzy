8da4ec70e77c3a0e59a4f5c8218cf37d58a1dea4
OwenBachyrycz <60482509+OwenBachyrycz@users.noreply.github.com>
2021-11-17 16:46:41 -0500

    Completed mecanum drivetrain TeleOp

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mecanumDrive.java
@@ -1,20 +1,61 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 /**
  * Create by Owen Bachyrycz on 11/17/2021.
  */
 
+@TeleOp(name="mecanumDrive", group="default")
+
 public class mecanumDrive extends OpMode {
+
+    DcMotor frontLeft;
+    DcMotor rearLeft;
+    DcMotor frontRight;
+    DcMotor rearRight;
+
+    double frontLeftPower;
+    double rearLeftPower;
+    double frontRightPower;
+    double rearRightPower;
 
     @Override
     public void init() {
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        rearLeft = hardwareMap.dcMotor.get("rearLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        rearRight = hardwareMap.dcMotor.get("rearRight");
 
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
     }
 
     @Override
     public void loop() {
+        double y = -gamepad1.left_stick_y;
+        double x = gamepad1.left_stick_x;
+        double rx = gamepad1.right_stick_x;
 
+        frontLeftPower = (y + x + rx);
+        rearLeftPower = (y - x + rx);
+        frontRightPower = (y - x - rx);
+        rearRightPower = (y + x -rx);
+
+        frontLeft.setPower(frontLeftPower);
+        rearLeft.setPower(rearLeftPower);
+        frontRight.setPower(frontRightPower);
+        rearRight.setPower(rearRightPower);
+    }
+
+    @Override
+    public void stop() {
+        frontLeft.setPower(0);
+        rearLeft.setPower(0);
+        frontRight.setPower(0);
+        rearRight.setPower(0);
     }
 }
