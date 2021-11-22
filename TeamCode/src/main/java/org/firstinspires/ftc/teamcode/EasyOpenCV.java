516ab7e745f5f18905f3e030dfb4c3b35f39c8fa
laptop <obachyrycz@gmail.com>
2021-11-22 11:51:25 -0500

    Added basic OpenCV OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EasyOpenCV.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.OpenCvCamera;
+import org.openftc.easyopencv.OpenCvCameraFactory;
+import org.openftc.easyopencv.OpenCvCameraRotation;
+
+/**
+ * Create by Owen Bachyrycz on 11/22/2021.
+ */
+
+@Autonomous(name = "EasyOpenCV", group = "Testing")
+
+public class EasyOpenCV extends LinearOpMode {
+
+    int cameraMonitorViewId = hardwareMap.appContext.getResources().getIdentifier("cameraMonitorViewId", "id", hardwareMap.appContext.getPackageName());
+    WebcamName webcam = hardwareMap.get(WebcamName.class, "webcam");
+    OpenCvCamera camera = OpenCvCameraFactory.getInstance().createWebcam(webcam, cameraMonitorViewId);
+
+    DcMotor frontLeft;
+    DcMotor rearLeft;
+    DcMotor frontRight;
+    DcMotor rearRight;
+    
+
+    @Override
+    public void runOpMode() {
+
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        rearLeft = hardwareMap.dcMotor.get("rearLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        rearRight = hardwareMap.dcMotor.get("rearRight");
+
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(1280, 720, OpenCvCameraRotation.UPSIDE_DOWN);
+            }
+            @Override
+            public void onError(int errorCode)
+            {
+                /*
+                 * This will be called if the camera could not be opened
+                 */
+            }
+        });
+        
+        
+        moveDrivetrain(0, 0, 0.5, 5000);
+
+    }
+
+    /**
+     * Sets drive train to move in x, y, and rx, for time milliseconds and then stops
+     * @param x horizontal speed
+     * @param y vertical speed
+     * @param rx rotational speed
+     * @param time how long to apply these movements for
+     */
+    public void moveDrivetrain(double x, double y, double rx, int time){
+        frontLeft.setPower(y + x + rx);
+        rearLeft.setPower(y - x + rx);
+        frontRight.setPower(y - x - rx);
+        rearRight.setPower(y + x -rx);
+        sleep(time);
+        stopDrivetrain();
+    }
+
+    public void stopDrivetrain() {
+        frontLeft.setPower(0);
+        rearLeft.setPower(0);
+        frontRight.setPower(0);
+        rearRight.setPower(0);
+    }
+
+
+}
