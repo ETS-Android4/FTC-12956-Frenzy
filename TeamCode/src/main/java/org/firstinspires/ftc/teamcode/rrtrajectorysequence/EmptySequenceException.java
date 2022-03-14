b9ff0a5620f8662bd37c7df603f8b09f899e57ce
owen <obachyrycz@gmail.com>
2022-03-14 09:38:08 -0400

    Reorganized Roadrunner file structure to make it easier to access

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/rrtrajectorysequence/EmptySequenceException.java
@@ -0,0 +1,4 @@
+package org.firstinspires.ftc.teamcode.rrtrajectorysequence;
+
+
+public class EmptySequenceException extends RuntimeException { }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tutorialOpMode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+public class tutorialOpMode extends OpMode {
+    @Override
+    public void init() {
+        //What the fuck am i supposed to do i dont know shit
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+}
